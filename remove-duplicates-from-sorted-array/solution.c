0a5457f640bc617967438382baff154c0b1e682a
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2015-11-24 14:56:05 +0800

    Add solution: Remove Duplicates from Sorted Array.

---
+++ remove-duplicates-from-sorted-array/solution.c
@@ -0,0 +1,9 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int i, j;
+    if (numsSize < 2)
+        return numsSize;
+    for (i = 0, j = 1; j < numsSize; ++j)
+        if (nums[i] != nums[j])
+            nums[++i] = nums[j];
+    return i + 1;
+}
