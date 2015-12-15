b278fe4d4ec8e27bbf6fab1ec736d8541dea3bc6
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2015-12-15 12:15:46 +0800

    Refine solution: Pow(x, n) in C.

--- powx-n/solution.c
+++ powx-n/solution.c
@@ -1,28 +1,17 @@
 double myPow(double x, int n) {
-    bool negative = false;
-    double base;
-    
-    if (n == 0)
-        return 1;
-    if (n == 1)
-        return x;
-    
-    if (n & 0x80000000) {
+    bool negative = n < 0;
+    double base, power;
+    if (negative) {
         n *= -1;
-        negative = true;
     }
-    
-    if (n & 0x00000001) {
-        base = myPow(x, (n - 1) / 2);
-        if (negative)
-            return 1 / (base * base * x);
-        else
-            return base * base * x;
-    } else {
-        base = myPow(x, n / 2);
-        if (negative)
-            return 1 / (base * base);
-        else
-            return base * base;
-    }
+    if (x == 0)
+        return n ? 0 : 1;
+    if (x == 1)
+        return 1;
+    if (x == -1)
+        return n & 0x00000001 ? -1 : 1;
+    for (base = x, power = 1.0; n; n >>= 1, base *= base)
+        if (n & 0x00000001)
+            power *= base;
+    return negative ? 1 / power : power;
 }
