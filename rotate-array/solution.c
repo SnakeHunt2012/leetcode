e2419f0f316a125904b2280348b6d0e8767a5cf1
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2015-12-18 16:20:46 +0800

    Add solution: Rotate Array in C.

---
+++ rotate-array/solution.c
@@ -0,0 +1,16 @@
+#define exch(A, B) { int temp = A; A = B; B = temp; }
+
+void rotate(int* nums, int numsSize, int k) {
+    int head, tail;
+    k %= numsSize;
+    if (k <= 0)
+        return;
+    if (k >= numsSize)
+        return;
+    for (head = 0, tail = numsSize - 1; head < tail; ++head, --tail)
+        exch(nums[head], nums[tail]);
+    for (head = 0, tail = k - 1; head < tail; ++head, --tail)
+        exch(nums[head], nums[tail]);
+    for (head = k, tail = numsSize - 1; head < tail; ++head, --tail)
+        exch(nums[head], nums[tail]);
+}
