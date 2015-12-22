5bb96afe98716580e9ae077f30df3a34a84d28dd
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2015-12-22 22:01:33 +0800

    Add solution: Copy List with Random Pointer in C.

---
+++ copy-list-with-random-pointer/solution.c
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * struct RandomListNode {
+ *     int label;
+ *     struct RandomListNode *next;
+ *     struct RandomListNode *random;
+ * };
+ */
+struct RandomListNode *copyRandomList(struct RandomListNode *head) {
+    struct RandomListNode *link, *node;
+    if (!head)
+        return NULL;
+    for (link = head; link; link = node->next) {
+        node = malloc(sizeof *node);
+        node->label = link->label;
+        node->next = link->next;
+        link->next = node;
+    }
+    for (link = head, node = head->next; node->next; link = node->next, node = link->next) {
+        node->random = link->random ? link->random->next : NULL;
+    }
+    node->random = link->random ? link->random->next : NULL;
+    
+    link = head;
+    head = head->next;
+    node = head;
+    link->next = link->next->next;
+    link = link->next;
+    while (link) {
+        node->next = link->next;
+        link->next = link->next->next;
+        link = link->next;
+        node = node->next;
+    }
+    node->next = NULL;
+    return head;
+}
